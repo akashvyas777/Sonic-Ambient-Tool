138d2a59d9ef35c29bceeb862048254b3a1903d6
akashvyas777 <akashvyas777@gmail.com>
2026-04-16 11:28:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+    // Required to be considered a PWA
+    e.respondWith(fetch(e.request));
 });
